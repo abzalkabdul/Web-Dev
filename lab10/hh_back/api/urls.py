39df4e7dc1fa121c08ce42cad00b070a4f8c6168
Abzal <k4bdoldaev@gmail.com>
2025-04-06 23:47:27 +0500

    upd: completed cbv

--- lab10/hh_back/api/urls.py
+++ lab10/hh_back/api/urls.py
@@ -4,8 +4,8 @@ from api import views
 urlpatterns = [
     path('companies/', views.C_ListAPIView.as_view()),
     path('companies/<int:company_id>/', views.GetCompanyAPIView.as_view()),
-    path('companies/<int:company_id>/vacancies/', views.c_vacancies_list),
-    path('vacancies/', views.vacancies_list),
+    path('companies/<int:company_id>/vacancies/', views.C_VacanciesListAPIView.as_view()),
+    path('vacancies/', views.VacanciesListAPIView.as_view()),
     path('vacancies/<int:vacancy_id>/', views.GetVacancyAPIView.as_view()),
-    path('vacancies/top_ten/', views.list_top_ten),
+    path('vacancies/top_ten/', views.ListTopTenAPIView.as_view()),
 ]
--- lab10/hh_back/api/views/cbv.py
+++ lab10/hh_back/api/views/cbv.py
@@ -8,98 +8,38 @@ from api.serializers import VacancySerializer, CompanySerializer
 from api.models import Company, Vacancy
 
 
-class C_ListAPIView(mixins.ListModelMixin,
-                    mixins.CreateModelMixin,
-
-                    generics.GenericAPIView):
+class C_ListAPIView(generics.ListCreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, args, kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, args, kwargs)
     
 
-class GetCompanyAPIView(APIView):
-    def get_object(self, company_id):
+class GetCompanyAPIView(generics.RetrieveAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    lookup_url_kwarg = 'company_id'
+
+
+class C_VacanciesListAPIView(generics.ListCreateAPIView):
+    serializer_class = VacancySerializer
+
+    def get_queryset(self):     #just magic
+        company_id = self.kwargs.get('company_id')
         try:
-            return Company.objects.get(pk=company_id)
+            company = Company.objects.get(pk=company_id)
         except Exception as e:
-            raise Response({'error': e}, status=status.HTTP_404_NOT_FOUND)
-        
-    def get(self, request, company_id):
-        company = self.get_object(company_id)
-        serializer = CompanySerializer(company)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            raise Response({'error': 'idiot'}, status=status.HTTP_404_NOT_FOUND)
+        return company.vacancy_set.all()
+
+
+class VacanciesListAPIView(generics.ListCreateAPIView):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
     
-    def post(self, request, company_id):
-        serializer = CompanySerializer(request.body)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    def put(self, request, company_id):
-        company = self.get_object(company_id)
-        serializer = CompanySerializer(instance=company, data=request.body)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class GetVacancyAPIView(generics.RetrieveUpdateDestroyAPIView):  
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+    lookup_url_kwarg='vacancy_id'
 
-    def delete(self, request, company_id):
-        company = self.get_object(company_id)
-        company.delete()
-        return Response({"message": f"Company with id-{company_id} is deleted"}, status=status.HTTP_204_NO_CONTENT)
-
-
-def c_vacancies_list(request, company_id):
-        company = Company.objects.get(pk=company_id)
-        c_vacancies_list = company.vacancy_set.all()
-        serializer = VacancySerializer(c_vacancies_list, many=True)
-        return JsonResponse(serializer.data, safe=False, json_dumps_params={'indent': 4})
-
-
-@api_view(['GET', 'POST'])
-def vacancies_list(request):
-    if request.method == "GET":
-        v_list = Vacancy.objects.all()
-        serializer = VacancySerializer(v_list, many=True, read_only=True)
-        return Response(serializer.data, status=status.HTTP_404_NOT_FOUND)
-
-    elif request.method == "POST":
-        serializer = VacancySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-class GetVacancyAPIView(APIView):
-    def get_object(self, vacancy_id):
-        try:
-            return Vacancy.objects.get(pk=vacancy_id)
-        except Exception as e:
-            raise Response({'error': e}, status=status.HTTP_404_NOT_FOUND)
-
-    def get(self, request, vacancy_id):
-        vacancy = self.get_object(vacancy_id)
-        serializer = VacancySerializer(vacancy)
-        return Response(serializer.data)
-
-    def put(self, request, vacancy_id):
-        vacancy = self.get_object(vacancy_id)
-        serializer = VacancySerializer(instance=vacancy, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, vacancy_id):
-        vacancy = self.get_object(vacancy_id)
-        vacancy.delete()
-        return Response({"message": f"Vacancy with id-{vacancy_id} is deleted"})
-
-def list_top_ten(request):
-    list_top_ten = Vacancy.objects.order_by("-salary")[:10]
-    ten_vacancies = VacancySerializer(list_top_ten, many=True)
-    return JsonResponse(ten_vacancies.data, safe=False, json_dumps_params={"indent":4})
+class ListTopTenAPIView(generics.ListAPIView): 
+    queryset = Vacancy.objects.order_by("-salary")[:10]
+    serializer_class = VacancySerializer
